5c73ad366df91ca4c18d9fb5dd601d09c5974882
Jeon Hyuncheol <julyfool@gmail.com>
2021-11-15 14:37:51 +0900

    📦 add Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.4
+import PackageDescription
+
+let package = Package(
+    name: "UIImage-ResizeMagick",
+    products: [
+        .library(name: "UIImage-ResizeMagick", targets: ["UIImage-ResizeMagick"])
+    ],
+    targets: [
+        .target(name: "UIImage-ResizeMagick", dependencies: [])
+    ]
+)
